2a32d60697690e02cd7d5525988889f92bf95bb7
Dan Prudhomme <dannyprud@yahoo.com>
2022-04-20 21:14:05 -0500

    Add framework to index.js, add db connect file in startup folder, add start script to server 'nodemone index.js'

---
+++ server/index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const cors = require('cors');
+const connectDB = require('./startup/db');
+// const users = require('./routes/users');
+// const tasks = require('./routes/tasks');
+const fs = require('fs');
+const path = require('path');
+
+
+const app = express();
+
+connectDB();
+
+app.use(cors());
+app.use(express.json());
+// app.use('/api/users', users);
+// app.use('/api/tasks', tasks);
+
+app.use('/uploads/images', express.static(path.join('uploads', 'images')));
+app.use((error, req, res, next) => {
+  if (req.file) {
+    fs.unlink(req.file.path, (err) => {
+        console.log(err);
+    });
+  }
+  if (res.headerSent) {
+    return (next(error))
+  }
+  res.status(error.code || 500);
+  res.json({ message: error.message || "An unknown error occured!"});
+});
+
+const port = process.env.PORT || 5000
+app.listen(port, () => {
+  console.log(`Server started on port: ${port}`);
+});
