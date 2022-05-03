863a530a2cea7c1fce15d5e6035eb6dad8545f79
Dan Prudhomme <dannyprud@yahoo.com>
2022-05-02 21:50:46 -0500

    Get a single user route test successful through Postman, verify auth is working, access not granted without JWT

--- server/routes/users.js
+++ server/routes/users.js
@@ -76,4 +76,18 @@ router.get("/", async (req, res) => {
   }
 });
 
+//Get a single user
+router.get("/:userId", auth, async (req, res) => {
+  try {
+    const user = await User.findById(req.params.userId);
+    if (!user)
+      return res
+        .status(400)
+        .send(`User with id ${req.params.userId} does not exist!`);
+    return res.send(user);
+  } catch (ex) {
+    return res.status(500).send(`Internal Server Error: ${ex}`);
+  }
+});
+
 module.exports = router;
